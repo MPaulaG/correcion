a5746800a96a22350b88fec6826bd7df59f779bc
MPaulaG <mpaula_12@live.com>
2022-12-07 07:55:03 -0500

    commit 1

---
+++ correcion/src/main/java/ec/edu/itsqmet/correccion/CorrecionApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.correccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrecionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrecionApplication.class, args);
+	}
+
+}
